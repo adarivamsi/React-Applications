c07ac8fb892f098e508f69d3656b355ab6fc7285
Vamsi Charan Adari <adarivamsi@gmail.com>
2020-08-15 23:51:42 -0400

    Added components.

--- assignment-day3/src/App.js
+++ assignment-day3/src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import EnquiryDetail from './components/EnquiryManager/EnquiryDetail/EnquiryDetail';
+import EnquiryManager from './components/EnquiryManager/EnquiryManager';
+import Home from './components/Home/Home';
+import Footer from './components/shared/footer/Footer';
+import Header from './components/shared/header/Header';
+import PageNotFound from './components/shared/PageNotFound/PageNotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <div className="row">
+            <div className="col">
+                <Header />
+            </div>
+        </div>
+
+        <div className="col">
+            <Switch>
+                <Route path="/" exact render={() => <Redirect to="/home" />} />                        
+                <Route path="/home" component={ Home } />                        
+                <Route path="/enquiries" component={ EnquiryManager } />
+                <Route path="/enquiries/:id" component={ EnquiryDetail } />
+                <Route path="/**" component={ PageNotFound } />
+            </Switch>
+        </div>
+
+        <div className="row">
+            <div className="col">
+                <Footer />
+            </div>
+        </div>
     </div>
   );
 }
--- assignment-day3/src/index.js
+++ assignment-day3/src/index.js
@@ -1,13 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+      <Route component={App} />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
