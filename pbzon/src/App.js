1add176f3b5422796391a912dab4c73db969f5ce
Vamsi Charan Adari <adarivamsi@gmail.com>
2020-10-10 22:29:06 -0400

    Added routes.

--- pbzon/src/App.js
+++ pbzon/src/App.js
@@ -1,24 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <div className="app">
-      <h1>Hey Vamsi Charan Adari!</h1>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout Page</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login Page</h1>
+          </Route>
+          <Route path="/">
+            <h1>Home Page!</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
-}
-
-{
-  /* baseurl  */
-}
-
-{
-  /* baseurl/checkout */
-}
-
-{
-  /* baseurl/login */
 }
 
 export default App;
