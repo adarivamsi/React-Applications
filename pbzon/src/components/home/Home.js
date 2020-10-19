59401b5697666402ca3ec39946f64dc75fc6a36a
Vamsi Charan Adari <adarivamsi@gmail.com>
2020-10-18 22:21:23 -0400

    Added product component.

--- pbzon/src/components/home/Home.js
+++ pbzon/src/components/home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Product from "../shared/product/Product";
 import "./Home.css";
 
 function Home() {
@@ -6,9 +7,57 @@ function Home() {
     <div className="home">
       <img
         className="home__image"
-        src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/YzExNTRjZmUt/YzExNTRjZmUt-ZWRmYzNiOWQt-w1500._CB418546933_.jpg"
+        src="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/2020/Other/RB-2884_SVOD_Halloween/Amazon_GW_DesktopTallHero_RB-2884_SVOD_V2_Halloween_1500x600._CB403043380_.jpg"
         alt=""
       />
+      <div className="home__row">
+        <Product
+          id="12321341"
+          title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+          price={11.96}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+        />
+        <Product
+          id="49538094"
+          title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+          price={239.0}
+          rating={4}
+          image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
+        />
+      </div>
+      <div className="home__row">
+        <Product
+          id="4903850"
+          title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+          price={199.99}
+          rating={3}
+          image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+        />
+        <Product
+          id="23445930"
+          title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+          price={98.99}
+          rating={5}
+          image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+        />
+        <Product
+          id="3254354345"
+          title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
+          price={598.99}
+          rating={4}
+          image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+        />
+      </div>
+      <div className="home__row">
+        <Product
+          id="90829332"
+          title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
+          price={1094.98}
+          rating={4}
+          image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
+        />
+      </div>
     </div>
   );
 }
