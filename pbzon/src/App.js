0f26bd1264cb00ddf562c01d18e509871498a1bc
Vamsi Charan Adari <adarivamsi@gmail.com>
2020-10-18 20:58:43 -0400

    Added banner.

--- pbzon/src/App.js
+++ pbzon/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Home from "./components/home/Home";
 import Header from "./components/shared/Header";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <div className="app">
         <Switch>
           <Route path="/checkout">
+            <Header />
             <h1>Checkout Page</h1>
           </Route>
           <Route path="/login">
@@ -16,7 +18,7 @@ function App() {
           </Route>
           <Route path="/">
             <Header />
-            <h1>Home Page!</h1>
+            <Home />
           </Route>
         </Switch>
       </div>
---
+++ pbzon/src/components/home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <img
+        className="home__image"
+        src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/YzExNTRjZmUt/YzExNTRjZmUt-ZWRmYzNiOWQt-w1500._CB418546933_.jpg"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default Home;
