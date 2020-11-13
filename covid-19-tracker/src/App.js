7ecde7caffd252661ed05f58374e35036e4cf936
Vamsi Charan Adari <adarivamsi@gmail.com>
2020-11-12 22:46:15 -0500

    Making API call and displaying them in loop for dropdown values.

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,9 +1,48 @@
-import './App.css';
+import { FormControl, MenuItem, Select } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+
+    getCountriesData();
+  }, []);
+
   return (
     <div className="app">
-      <h1>Covid 19 Project! On the Way.</h1>
+      <div className="app__header">
+        <h1>Covid 19 Tracker - Passionate Burger</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      {/* {Header} */}
+      {/* Title + Select input dropdown field */}
+
+      {/* Infoboxes */}
+      {/* Infoboxes */}
+      {/* Infoboxes */}
+
+      {/* Table */}
+      {/* Graph */}
+      {/* Map */}
     </div>
   );
 }
