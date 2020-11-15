9cd4036366b6c116144ccc18979a8bdf525e10c2
Vamsi Charan Adari <adarivamsi@gmail.com>
2020-11-15 15:18:30 -0500

    changes

--- assignment-day3/src/components/EnquiryManager/EnquiryDetail/EnquiryDetail.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-class EnquiryDetail extends Component {
-    render() {
-        return <p>EnquiryDetail Works!</p>
-    }
-}
-
-export default EnquiryDetail
--- assignment-day3/src/components/Home/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-class Home extends Component {
-    render() {
-        return <p>Home Works!</p>
-    }
-}
-
-export default Home
